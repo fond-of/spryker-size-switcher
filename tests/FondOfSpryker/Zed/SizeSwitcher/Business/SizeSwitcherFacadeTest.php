a1c01689881986477ea49ae054b222675805f642
Pascal Fischer <p-fischer@gmx.net>
2020-10-05 09:33:20 +0200

    unit tests

--- tests/FondOfSpryker/Zed/SizeSwitcher/Business/SizeSwitcherFacadeTest.php
+++ tests/FondOfSpryker/Zed/SizeSwitcher/Business/SizeSwitcherFacadeTest.php
@@ -3,9 +3,7 @@
 namespace FondOfSpryker\Zed\SizeSwitcher\Business;
 
 use Codeception\Test\Unit;
-use FondOfSpryker\Zed\SizeSwitcher\Dependency\Facade\SizeSwitcherToEventBehaviorFacadeBridge;
-use FondOfSpryker\Zed\SizeSwitcher\Dependency\Facade\SizeSwitcherToProductPageSearchFacadeBridge;
-use FondOfSpryker\Zed\SizeSwitcher\Persistence\SizeSwitcherRepository;
+use FondOfSpryker\Zed\SizeSwitcher\Business\Publisher\ProductAbstractPageSearchPublisher;
 
 class SizeSwitcherFacadeTest extends Unit
 {
@@ -20,19 +18,9 @@ class SizeSwitcherFacadeTest extends Unit
     protected $factoryMock;
 
     /**
-     * @var \FondOfSpryker\Zed\SizeSwitcher\Persistence\SizeSwitcherRepository|\PHPUnit\Framework\MockObject\MockObject
+     * @var \FondOfSpryker\Zed\SizeSwitcher\Business\Publisher\ProductAbstractPageSearchPublisher|\PHPUnit\Framework\MockObject\MockObject
      */
-    protected $repositoryMock;
-
-    /**
-     * @var \FondOfSpryker\Zed\SizeSwitcher\Dependency\Facade\SizeSwitcherToEventBehaviorFacadeBridge|\PHPUnit\Framework\MockObject\MockObject
-     */
-    protected $eventBehaviorFacadeBridgeMock;
-
-    /**
-     * @var \FondOfSpryker\Zed\SizeSwitcher\Dependency\Facade\SizeSwitcherToProductPageSearchFacadeBridge|\PHPUnit\Framework\MockObject\MockObject
-     */
-    protected $productPageSearchFacadeBridgeMock;
+    protected $publisher;
 
     /**
      * @return void
@@ -45,21 +33,12 @@ class SizeSwitcherFacadeTest extends Unit
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->repositoryMock = $this->getMockBuilder(SizeSwitcherRepository::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $this->eventBehaviorFacadeBridgeMock = $this->getMockBuilder(SizeSwitcherToEventBehaviorFacadeBridge::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $this->productPageSearchFacadeBridgeMock = $this->getMockBuilder(SizeSwitcherToProductPageSearchFacadeBridge::class)
+        $this->publisher = $this->getMockBuilder(ProductAbstractPageSearchPublisher::class)
             ->disableOriginalConstructor()
             ->getMock();
 
         $this->facade = new SizeSwitcherFacade();
         $this->facade->setFactory($this->factoryMock);
-        $this->facade->setRepository($this->repositoryMock);
     }
 
     /**
@@ -67,34 +46,19 @@ class SizeSwitcherFacadeTest extends Unit
      */
     public function testUpdate()
     {
-        /*$this->factoryMock->expects($this->once())
-            ->method('getEventBehaviorFacade')
-            ->willReturn($this->eventBehaviorFacadeBridgeMock);
-
-        $this->eventBehaviorFacadeBridgeMock->expects($this->once())
-            ->method('getEventTransferIds')
-            ->willReturn([1, 2, 3]);
-
-        $this->repositoryMock->expects($this->once())
-            ->method('queryProductAbstractSkusByAvailabilityIds')
-            ->willReturn(['SKU-1', 'SKU-2', 'SKU-3']);
-
-        $this->repositoryMock->expects($this->once())
-            ->method('queryProductAbstractIdsBySku')
-            ->willReturn([11, 22, 33]);
-
-        $this->factoryMock->expects($this->once())
-            ->method('getProductPageSearchFacade')
-            ->willReturn($this->productPageSearchFacadeBridgeMock);
-
-        $this->productPageSearchFacadeBridgeMock->expects($this->once())
-            ->method('publish');
-
         $eventEntityTransfer = include codecept_data_dir('EventEntityTransfer.php');
         $eventEntityTransferCollection = [
             $eventEntityTransfer,
         ];
 
-        $this->facade->update($eventEntityTransferCollection);*/
+        $this->factoryMock->expects($this->once())
+            ->method('createProductAbstractPageSearchPublisher')
+            ->willReturn($this->publisher);
+
+        $this->publisher->expects($this->once())
+            ->method('publish')
+            ->with($eventEntityTransferCollection);
+
+        $this->facade->update($eventEntityTransferCollection);
     }
 }
